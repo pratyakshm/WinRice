7a19183a34b180ecffb776992bd7f7987c5f6eaa
pratyakshm <pratyakshm@protonmail.com>
2020-10-25 16:05:46 +0530

    CW: app installs: add discord (optional)

--- CleanWin.ps1
+++ CleanWin.ps1
@@ -1115,6 +1115,21 @@ Function InstallDesktopEditors {
 		}
 }
 
+# Install Discord
+Function InstallDiscord {
+	Write-Output " "
+	$question = 'Do you want to install Discord?'
+	$choices = New-Object Collections.ObjectModel.Collection[Management.Automation.Host.ChoiceDescription]
+	$choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&Yes'))
+	$choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&No'))
+	$decision = $Host.UI.PromptForChoice($message, $question, $choices, 1)
+		if ($decision -eq 0) {
+			Write-Output "Installing Discord..."
+			winget install --id=Discord.Discord --silent
+		}
+}
+
+
 # Install Firefox
 Function InstallFirefox {
 	Write-Output " "
