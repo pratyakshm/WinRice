00a6e7a56d5039e9c208f62ea0d167446bf15336
Pratyaksh Mehrotra <pratyakshm@protonmail.com>
2021-02-03 16:48:29 +0530

    Apps & Features: Install Choclatey: replace 'iex' alias with full content 'Invoke-Expression
    Signed-off-by: Pratyaksh Mehrotra <pratyakshm@protonmail.com>

--- CleanWin-GUI.ps1
+++ CleanWin-GUI.ps1
@@ -770,7 +770,7 @@ $START_MENU_LAYOUT = @"
 
 $InstallChoco.Add_Click( {
     Write-Host "Installing Chocolatey package manager..."
-	Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+	Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 	choco install chocolatey-core.extension -y
 	Write-Host "Done."
 	Start-Sleep 1
