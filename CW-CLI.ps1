ce9acfa93475d9a739c64a9fa7dfa1ebfb527b3d
Pratyaksh Mehrotra <pratyakshm@protonmail.com>
2021-03-09 17:08:38 +0530

    CLI: AppsFeatures: InstallWinGet: update messages
    Signed-off-by: Pratyaksh Mehrotra <pratyakshm@protonmail.com>

--- CW-CLI.ps1
+++ CW-CLI.ps1
@@ -419,10 +419,10 @@ Function InstallWinGet {
     # If the file exists, proceed with downloading WinGet files, else inform user about no internet connection.
     If (Test-Path NetTestFile) {
         Remove-Item NetTestFile
-        Write-Host "Downloading required files..."
+        Write-Host "Downloading WinGet installation files..."
         Start-BitsTransfer https://github.com/CleanWin/Files/raw/main/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle
         Start-BitsTransfer https://github.com/CleanWin/Files/raw/main/Microsoft.VCLibs.140.00.UWPDesktop_14.0.29231.0_x64__8wekyb3d8bbwe.Appx
-        Write-Host "Installing Windows Package Manager (WinGet)..."
+        Write-Host "Installing WinGet..."
         Add-AppxPackage -Path .\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle -DependencyPath .\Microsoft.VCLibs.140.00.UWPDesktop_14.0.29231.0_x64__8wekyb3d8bbwe.Appx
         Remove-Item Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle
         Remove-Item Microsoft.VCLibs.140.00.UWPDesktop_14.0.29231.0_x64__8wekyb3d8bbwe.Appx
