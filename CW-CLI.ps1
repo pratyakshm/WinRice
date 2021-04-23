10f5291626b5d49b45c2b5e1fb45bae825b2e096
Pratyaksh Mehrotra <pratyakshm@protonmail.com>
2021-04-23 14:38:59 +0530

    AP: add OneDrive 64-bit uninstall to GUI and uncomment it
    Signed-off-by: Pratyaksh Mehrotra <pratyakshm@protonmail.com>

--- CW-CLI.ps1
+++ CW-CLI.ps1
@@ -373,12 +373,14 @@ $ErrorActionPreference = 'SilentlyContinue'
 		New-Item $ExplorerReg2 | Out-Null
 	}
 	Set-ItemProperty $ExplorerReg2 System.IsPinnedToNameSpaceTree -Value 0
+
 	# For 64-bit versions of Microsoft OneDrive (in preview)
-	# Remove-Item "C:\Users\$env:UserName\AppData\Local\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\settings.json"
-	# Remove-Item "C:\Users\$env:UserName\AppData\Local\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\settings.json.backup" -ErrorAction SilentlyContinue
-	# Start-BitsTransfer -Source "https://raw.githubusercontent.com/CleanWin/Files/main/settings.json" -Destination "C:\Users\$env:UserName\AppData\Local\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\settings.json"
-	# winget uninstall OneDriveSetup.exe
+	Remove-Item "C:\Users\$env:UserName\AppData\Local\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\settings.json"
+	Remove-Item "C:\Users\$env:UserName\AppData\Local\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\settings.json.backup" -ErrorAction SilentlyContinue
+	Start-BitsTransfer -Source "https://raw.githubusercontent.com/CleanWin/Files/main/settings.json" -Destination "C:\Users\$env:UserName\AppData\Local\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\settings.json"
+	winget uninstall OneDriveSetup.exe | Out-Null
 	Remove-Item env:OneDrive
+
 	Write-Host "Done."
 }
 
