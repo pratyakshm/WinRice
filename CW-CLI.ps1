fad8da4118701f3b497a03e2a09b2ce344ea8585
Pratyaksh Mehrotra <pratyakshm@protonmail.com>
2021-05-06 21:27:44 +0530

    CLI;AP: add Windows Photo Viewer to Open with menu because its better than the current Photos app
    Signed-off-by: Pratyaksh Mehrotra <pratyakshm@protonmail.com>

--- CW-CLI.ps1
+++ CW-CLI.ps1
@@ -17,7 +17,7 @@ $tasks = @(
 	"DebloatApps", "UnpinStartTiles", "InstallWinGet", "UninstallOneDrive", "CleanupRegistry", 
 	"DisableBrowserRestoreAd",      # "EnableBrowserRestoreAd",
 	"UninstallFeatures", "EnableWSL", "EnabledotNET3.5", # "EnableSandbox",
-	"Install7zip", "Winstall", 
+	"Install7zip", "Winstall", "SetPhotoViewerAssociation", # "SetPhotoViewerAssociation",
 	"ChangesDone",
 
 ### Privacy & Security ###
@@ -560,6 +560,34 @@ Function Winstall {
     }
 }
 
+# Set Windows Photo Viewer association for bmp, gif, jpg, png and tif
+Function SetPhotoViewerAssociation {
+	Write-Output "Setting Photo Viewer association for bmp, gif, jpg, png and tif..."
+	If (!(Test-Path "HKCR:")) {
+		New-PSDrive -Name "HKCR" -PSProvider "Registry" -Root "HKEY_CLASSES_ROOT" | Out-Null
+	}
+	ForEach ($type in @("Paint.Picture", "giffile", "jpegfile", "pngfile")) {
+		New-Item -Path $("HKCR:\$type\shell\open") -Force | Out-Null
+		New-Item -Path $("HKCR:\$type\shell\open\command") | Out-Null
+		Set-ItemProperty -Path $("HKCR:\$type\shell\open") -Name "MuiVerb" -Type ExpandString -Value "@%ProgramFiles%\Windows Photo Viewer\photoviewer.dll,-3043"
+		Set-ItemProperty -Path $("HKCR:\$type\shell\open\command") -Name "(Default)" -Type ExpandString -Value "%SystemRoot%\System32\rundll32.exe `"%ProgramFiles%\Windows Photo Viewer\PhotoViewer.dll`", ImageView_Fullscreen %1"
+	}
+}
+
+# Unset Windows Photo Viewer association for bmp, gif, jpg, png and tif
+Function UnsetPhotoViewerAssociation {
+	Write-Output "Unsetting Photo Viewer association for bmp, gif, jpg, png and tif..."
+	If (!(Test-Path "HKCR:")) {
+		New-PSDrive -Name "HKCR" -PSProvider "Registry" -Root "HKEY_CLASSES_ROOT" | Out-Null
+	}
+	Remove-Item -Path "HKCR:\Paint.Picture\shell\open" -Recurse -ErrorAction SilentlyContinue
+	Remove-ItemProperty -Path "HKCR:\giffile\shell\open" -Name "MuiVerb" -ErrorAction SilentlyContinue
+	Set-ItemProperty -Path "HKCR:\giffile\shell\open" -Name "CommandId" -Type String -Value "IE.File"
+	Set-ItemProperty -Path "HKCR:\giffile\shell\open\command" -Name "(Default)" -Type String -Value "`"$env:SystemDrive\Program Files\Internet Explorer\iexplore.exe`" %1"
+	Set-ItemProperty -Path "HKCR:\giffile\shell\open\command" -Name "DelegateExecute" -Type String -Value "{17FE9752-0B5A-4665-84CD-569794602F5C}"
+	Remove-Item -Path "HKCR:\jpegfile\shell\open" -Recurse -ErrorAction SilentlyContinue
+	Remove-Item -Path "HKCR:\pngfile\shell\open" -Recurse -ErrorAction SilentlyContinue
+}
 
 
 ######################################
