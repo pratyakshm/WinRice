1d1a98fd1497865befb1c8eafe85b31a388ed6c4
Pratyaksh Mehrotra <pratyakshm@protonmail.com>
2021-06-04 19:25:20 +0530

    TS: remove DoSvc registry modification since it results in WinGet not working for a small batch of users (#23)

--- CW-GUI.ps1
+++ CW-GUI.ps1
@@ -1408,20 +1408,17 @@ $ErrorActionPreference = 'SilentlyContinue'
         "Delay feature updates by 20 days"
         "Delay quality updates by 4 days"
         "Turn off re-installation of bloatware after Windows Updates"
-        "Download Windows Update and other Microsoft Software only from Microsoft CDN"
     )
     ForEach ($WinUpdatePolicy in $WinUpdatePolicies) {
         Write-Host "    - $WinUpdatePolicy"
     }
 
-    $DeliveryOptimization = "HKLM:\SYSTEM\CurrentControlSet\Services\DoSvc"
     $Update1 = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate"
     $Update2 = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU"
         If (!(Test-Path $Update1)) {
         New-Item -Path $Update1 | Out-Null
         New-Item -Path $Update2 | Out-Null
         }
-    Set-ItemProperty -Path $DeliveryOptimization -Name Start -Type DWord -Value 4
     Set-ItemProperty -Path $Update1 -Name DeferQualityUpdates -Type DWord -Value 1
     Set-ItemProperty -Path $Update1 -Name DeferQualityUpdatesPeriodInDays -Type DWord -Value 4
     Set-ItemProperty -Path $Update1 -Name DeferFeatureUpdates -Type DWord -Value 1
