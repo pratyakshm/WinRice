1faa98a565ebee3a1f502791f1e75451009eb4c2
Pratyaksh Mehrotra <pratyakshm@protonmail.com>
2021-06-26 18:13:04 +0530

    AP: UnpinStartTiles: add Windows 11 check

--- CW-CLI.ps1
+++ CW-CLI.ps1
@@ -288,69 +288,80 @@ $ErrorActionPreference = 'SilentlyContinue'
 # Unpin all start menu tiles.
 Function UnpinStartTiles {
 	Write-Host " "
-	Write-Host "Unpinning all tiles from Start Menu..."
-	Set-Content -Path 'C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\DefaultLayouts.xml' -Value '<LayoutModificationTemplate xmlns:defaultlayout="http://schemas.microsoft.com/Start/2014/FullDefaultLayout" xmlns:start="http://schemas.microsoft.com/Start/2014/StartLayout" Version="1" xmlns="http://schemas.microsoft.com/Start/2014/LayoutModification">'
-	Add-Content -Path 'C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\DefaultLayouts.xml' -value '  <LayoutOptions StartTileGroupCellWidth="6" />'
-	Add-Content -Path 'C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\DefaultLayouts.xml' -value '  <DefaultLayoutOverride>'
-	Add-Content -Path 'C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\DefaultLayouts.xml' -value '    <StartLayoutCollection>'
-	Add-Content -Path 'C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\DefaultLayouts.xml' -value '      <defaultlayout:StartLayout GroupCellWidth="6" />'
-	Add-Content -Path 'C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\DefaultLayouts.xml' -value '    </StartLayoutCollection>'
-	Add-Content -Path 'C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\DefaultLayouts.xml' -value '  </DefaultLayoutOverride>'
-	Add-Content -Path 'C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\DefaultLayouts.xml' -value '    <CustomTaskbarLayoutCollection>'
-	Add-Content -Path 'C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\DefaultLayouts.xml' -value '      <defaultlayout:TaskbarLayout>'
-	Add-Content -Path 'C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\DefaultLayouts.xml' -value '        <taskbar:TaskbarPinList>'
-	Add-Content -Path 'C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\DefaultLayouts.xml' -value '          <taskbar:UWA AppUserModelID="Microsoft.MicrosoftEdge_8wekyb3d8bbwe!MicrosoftEdge" />'
-	Add-Content -Path 'C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\DefaultLayouts.xml' -value '          <taskbar:DesktopApp DesktopApplicationLinkPath="%APPDATA%\Microsoft\Windows\Start Menu\Programs\System Tools\File Explorer.lnk" />'
-	Add-Content -Path 'C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\DefaultLayouts.xml' -value '        </taskbar:TaskbarPinList>'
-	Add-Content -Path 'C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\DefaultLayouts.xml' -value '      </defaultlayout:TaskbarLayout>'
-	Add-Content -Path 'C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\DefaultLayouts.xml' -value '    </CustomTaskbarLayoutCollection>'
-	Add-Content -Path 'C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\DefaultLayouts.xml' -value '</LayoutModificationTemplate>'
-	$START_MENU_LAYOUT = @"
-	<LayoutModificationTemplate xmlns:defaultlayout="http://schemas.microsoft.com/Start/2014/FullDefaultLayout" xmlns:start="http://schemas.microsoft.com/Start/2014/StartLayout" Version="1" xmlns:taskbar="http://schemas.microsoft.com/Start/2014/TaskbarLayout" xmlns="http://schemas.microsoft.com/Start/2014/LayoutModification">
-		<LayoutOptions StartTileGroupCellWidth="6" />
-		<DefaultLayoutOverride>
-			<StartLayoutCollection>
-				<defaultlayout:StartLayout GroupCellWidth="6" />
-			</StartLayoutCollection>
-		</DefaultLayoutOverride>
-	</LayoutModificationTemplate>
+	$CurrentVersionPath = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion'
+	$ProductName = Get-ItemPropertyValue $CurrentVersionPath -Name ProductName
+	if ($ProductName -match "Windows 10") {
+		Write-Host "Unpinning all tiles from Start Menu..."
+		Set-Content -Path 'C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\DefaultLayouts.xml' -Value '<LayoutModificationTemplate xmlns:defaultlayout="http://schemas.microsoft.com/Start/2014/FullDefaultLayout" xmlns:start="http://schemas.microsoft.com/Start/2014/StartLayout" Version="1" xmlns="http://schemas.microsoft.com/Start/2014/LayoutModification">'
+		Add-Content -Path 'C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\DefaultLayouts.xml' -value '  <LayoutOptions StartTileGroupCellWidth="6" />'
+		Add-Content -Path 'C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\DefaultLayouts.xml' -value '  <DefaultLayoutOverride>'
+		Add-Content -Path 'C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\DefaultLayouts.xml' -value '    <StartLayoutCollection>'
+		Add-Content -Path 'C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\DefaultLayouts.xml' -value '      <defaultlayout:StartLayout GroupCellWidth="6" />'
+		Add-Content -Path 'C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\DefaultLayouts.xml' -value '    </StartLayoutCollection>'
+		Add-Content -Path 'C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\DefaultLayouts.xml' -value '  </DefaultLayoutOverride>'
+		Add-Content -Path 'C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\DefaultLayouts.xml' -value '    <CustomTaskbarLayoutCollection>'
+		Add-Content -Path 'C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\DefaultLayouts.xml' -value '      <defaultlayout:TaskbarLayout>'
+		Add-Content -Path 'C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\DefaultLayouts.xml' -value '        <taskbar:TaskbarPinList>'
+		Add-Content -Path 'C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\DefaultLayouts.xml' -value '          <taskbar:UWA AppUserModelID="Microsoft.MicrosoftEdge_8wekyb3d8bbwe!MicrosoftEdge" />'
+		Add-Content -Path 'C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\DefaultLayouts.xml' -value '          <taskbar:DesktopApp DesktopApplicationLinkPath="%APPDATA%\Microsoft\Windows\Start Menu\Programs\System Tools\File Explorer.lnk" />'
+		Add-Content -Path 'C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\DefaultLayouts.xml' -value '        </taskbar:TaskbarPinList>'
+		Add-Content -Path 'C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\DefaultLayouts.xml' -value '      </defaultlayout:TaskbarLayout>'
+		Add-Content -Path 'C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\DefaultLayouts.xml' -value '    </CustomTaskbarLayoutCollection>'
+		Add-Content -Path 'C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\DefaultLayouts.xml' -value '</LayoutModificationTemplate>'
+		$START_MENU_LAYOUT = @"
+		<LayoutModificationTemplate xmlns:defaultlayout="http://schemas.microsoft.com/Start/2014/FullDefaultLayout" xmlns:start="http://schemas.microsoft.com/Start/2014/StartLayout" Version="1" xmlns:taskbar="http://schemas.microsoft.com/Start/2014/TaskbarLayout" xmlns="http://schemas.microsoft.com/Start/2014/LayoutModification">
+			<LayoutOptions StartTileGroupCellWidth="6" />
+			<DefaultLayoutOverride>
+				<StartLayoutCollection>
+					<defaultlayout:StartLayout GroupCellWidth="6" />
+				</StartLayoutCollection>
+			</DefaultLayoutOverride>
+		</LayoutModificationTemplate>
 "@
-	$layoutFile="C:\Windows\StartMenuLayout.xml"
-	# Delete layout file if it already exists.
-	If(Test-Path $layoutFile)
-	{
-		Remove-Item $layoutFile
-	}
-	# Creates a blank layout file.
-	$START_MENU_LAYOUT | Out-File $layoutFile -Encoding ASCII
-	$regAliases = @("HKLM", "HKCU")
-	# Assign the start layout and force it to apply with "LockedStartLayout" at both the machine and user level.
-	foreach ($regAlias in $regAliases){
-		$basePath = $regAlias + ":\SOFTWARE\Policies\Microsoft\Windows"
-		$keyPath = $basePath + "\Explorer" 
-		IF(!(Test-Path -Path $keyPath)) { 
-			New-Item -Path $basePath -Name "Explorer" | Out-Null
+		$layoutFile="C:\Windows\StartMenuLayout.xml"
+		# Delete layout file if it already exists.
+		If(Test-Path $layoutFile)
+		{
+			Remove-Item $layoutFile
 		}
-		Set-ItemProperty -Path $keyPath -Name "LockedStartLayout" -Value 1
-		Set-ItemProperty -Path $keyPath -Name "StartLayoutFile" -Value $layoutFile
+		# Creates a blank layout file.
+		$START_MENU_LAYOUT | Out-File $layoutFile -Encoding ASCII
+		$regAliases = @("HKLM", "HKCU")
+		# Assign the start layout and force it to apply with "LockedStartLayout" at both the machine and user level.
+		foreach ($regAlias in $regAliases){
+			$basePath = $regAlias + ":\SOFTWARE\Policies\Microsoft\Windows"
+			$keyPath = $basePath + "\Explorer" 
+			IF(!(Test-Path -Path $keyPath)) { 
+				New-Item -Path $basePath -Name "Explorer" | Out-Null
+			}
+			Set-ItemProperty -Path $keyPath -Name "LockedStartLayout" -Value 1
+			Set-ItemProperty -Path $keyPath -Name "StartLayoutFile" -Value $layoutFile
+		}
+		# Restart Explorer, open the start menu (necessary to load the new layout), and give it a few seconds to process.
+		Stop-Process -name explorer -Force
+		Start-Sleep -s 5
+		$wshell = New-Object -ComObject wscript.shell; $wshell.SendKeys('^{ESCAPE}')
+		Start-Sleep -s 5
+		# Enable the ability to pin items again by disabling "LockedStartLayout".
+		foreach ($regAlias in $regAliases){
+			$basePath = $regAlias + ":\SOFTWARE\Policies\Microsoft\Windows"
+			$keyPath = $basePath + "\Explorer" 
+			Set-ItemProperty -Path $keyPath -Name "LockedStartLayout" -Value 0
+		}
+		# Restart Explorer and delete the layout file.
+		Stop-Process -name explorer -Force
+		# Uncomment the next line to make clean start menu default for all new users.
+		Import-StartLayout -LayoutPath $layoutFile -MountPath $env:SystemDrive\
+		Remove-Item $layoutFile
+		Write-Host "Unpinned all tiles from Start Menu."
 	}
-	# Restart Explorer, open the start menu (necessary to load the new layout), and give it a few seconds to process.
-	Stop-Process -name explorer -Force
-	Start-Sleep -s 5
-	$wshell = New-Object -ComObject wscript.shell; $wshell.SendKeys('^{ESCAPE}')
-	Start-Sleep -s 5
-	# Enable the ability to pin items again by disabling "LockedStartLayout".
-	foreach ($regAlias in $regAliases){
-		$basePath = $regAlias + ":\SOFTWARE\Policies\Microsoft\Windows"
-		$keyPath = $basePath + "\Explorer" 
-		Set-ItemProperty -Path $keyPath -Name "LockedStartLayout" -Value 0
+	elseif ($ProductName -match "Windows 11") {
+		Write-Host "This device is currently on $ProductName"
+		Write-Host "CleanWin currently cannot unpin apps from Start Menu in Windows 11."
 	}
-	# Restart Explorer and delete the layout file.
-	Stop-Process -name explorer -Force
-	# Uncomment the next line to make clean start menu default for all new users.
-	Import-StartLayout -LayoutPath $layoutFile -MountPath $env:SystemDrive\
-	Remove-Item $layoutFile
-	Write-Host "Unpinned all tiles from Start Menu."
+	else {
+		# Do nothing.
+	}
 }
 
 # Unpin Apps from taskbar (https://docs.microsoft.com/en-us/answers/questions/214599/unpin-icons-from-taskbar-in-windows-10-20h2.html).
