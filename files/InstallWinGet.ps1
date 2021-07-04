5fb2cd490317adfb34558148462a210f6001e2c5
Pratyaksh Mehrotra <pratyakshm@protonmail.com>
2021-07-04 19:39:14 +0530

    files: InstallWinGet: update

--- files/InstallWinGet.ps1
+++ files/InstallWinGet.ps1
@@ -1,22 +1,43 @@
-# This file is a part of the CleanWin software
-# Copyright (c) 2021 PratyakshM <pratyakshm@protonmail.com>
-# All rights reserved.
+$ErrorActionPreference = 'SilentlyContinue'
+$ProgressPreference = 'SilentlyContinue'
+	Write-Host " "
+	if (!(Get-Command winget)) {
+		Write-Host "Preparing download..."
+		# Create new folder and set location.
+		if (!(Test-Path CleanWin)) {
+			New-Item CleanWin -ItemType Directory | out-Null
+			$currentdir = $(Get-Location).Path
+			$dir = "$currentdir/CleanWin"
+			Set-Location $dir
+		}
+		else {
+			Set-Location CleanWin
+		}
 
-# Import BitsTransfer module and download NetTestFile
-Import-Module BitsTransfer 
-Start-BitsTransfer https://raw.githubusercontent.com/CleanWin/Files/main/NetTestFile
-# If the file exists, proceed with downloading WinGet files, else inform user about no internet connection.
-If (Test-Path NetTestFile) {
-    Remove-Item NetTestFile
-    Write-Host "Downloading required files..."
-    Start-BitsTransfer https://github.com/CleanWin/Files/raw/main/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle
-    Start-BitsTransfer https://github.com/CleanWin/Files/raw/main/Microsoft.VCLibs.140.00.UWPDesktop_14.0.29231.0_x64__8wekyb3d8bbwe.Appx
-    Write-Host "Installing Windows Package Manager (WinGet)..."
-    Add-AppxPackage -Path .\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle -DependencyPath .\Microsoft.VCLibs.140.00.UWPDesktop_14.0.29231.0_x64__8wekyb3d8bbwe.Appx
-    Remove-Item Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle
-    Remove-Item Microsoft.VCLibs.140.00.UWPDesktop_14.0.29231.0_x64__8wekyb3d8bbwe.Appx
-    Write-Host "Done."
-	} 
+		# Download the packages.
+		Import-Module BitsTransfer
+		$WinGetURL = Invoke-RestMethod -Uri "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
+		$VCLibsURL = "https://github.com/CleanWin/Files/raw/main/Microsoft.VCLibs.140.00.UWPDesktop_14.0.29231.0_x64__8wekyb3d8bbwe.Appx"
+		Write-Host "Downloading WinGet installation packages..."
+		Start-BitsTransfer $WinGetURL.assets.browser_download_url ; Start-BitsTransfer $VCLibsURL 
+
+		# Install WinGet.
+		Write-Host "Installing WinGet..."
+		Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -DependencyPath Microsoft.VCLibs.140.00.UWPDesktop_14.0.29231.0_x64__8wekyb3d8bbwe.Appx
+			
+		# Cleanup installers.
+		Set-Location ..
+		Remove-Item CleanWin -Recurse -Force
+
+		# Get-Command winget, if it works then print success message.
+		if (Get-Command winget) {
+			Write-Host "Installed WinGet."
+		}
+		else {
+			Write-Host "WinGet could not be installed."
+		}
+
+	}
 	else {
-	  Write-Host "We can't connect to GitHub to download the installation files. Are you sure that your internet connection is working?"
-    }
+		Write-Host "WinGet is already installed on this device."
+	}
