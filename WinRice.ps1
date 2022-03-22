648dd7de386b2d702e88e334309e773114b0c016
Pratyaksh Mehrotra <pratyakshm@protonmail.com>
2022-03-22 20:49:36 +0530

    OS: Added RestorePowerOptions to restore hidden power options in Advanced power settings menu, closes #51

--- WinRice.ps1
+++ WinRice.ps1
@@ -73,7 +73,7 @@ $tasks = @(
 	"EnableClipboard",				
 	# "DisableClipboard",
 
-	### Security ###
+### Security ###
 	# "AutoLoginPostUpdate", 		    
 	"StayOnLockscreenPostUpdate",
 	# "DisableVBS",
@@ -100,6 +100,7 @@ $tasks = @(
 	# "EnableStorageSense",
 	# "DisableReserves",	   
 	# "EnableReserves",
+	"RestorePowerOptions",
 	"DisableAutoplay",             
 	# "EnableAutoplay",
 	"DisableAutorun",              
@@ -2899,6 +2900,23 @@ Function OS {
 	print "---------------------------"
 	print "      OPERATING SYSTEM     "   
 	print "---------------------------"
+}
+
+# Restore Power Options
+Function RestorePowerOptions {
+	space
+	print "Restoring power options in Advanced power settings menu..."
+	New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\0012ee47-9041-4b5d-9b77-535fba8b1442\6738e2c4-e8a5-4a42-b16a-e040e769756e" -Name Attributes -Type DWord -Value 0 -Force | Out-Null
+	New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\F15576E8-98B7-4186-B944-EAFA664402D9" -Name Attributes -Type DWord -Value 0 -Force | Out-Null
+	New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\19cbb8fa-5279-450e-9fac-8a3d5fedd0c1\12bbebe6-58d6-4636-95bb-3217ef867c1a"  -Name Attributes -Type DWord -Value 0 -Force | Out-Null
+	New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\2a737441-1930-4402-8d77-b2bebba308a3\48e6b7a6-50f5-4782-a5d4-53bb8f07e226"  -Name Attributes -Type DWord -Value 0 -Force | Out-Null
+	New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\54533251-82be-4824-96c1-47b60b740d00\bc5038f7-23e0-4960-96da-33abaf5935ec"  -Name Attributes -Type DWord -Value 0 -Force | Out-Null
+	New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\54533251-82be-4824-96c1-47b60b740d00\893dee8e-2bef-41e0-89c6-b55d0929964c"  -Name Attributes -Type DWord -Value 0 -Force | Out-Null
+	New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\54533251-82be-4824-96c1-47b60b740d00\94D3A615-A899-4AC5-AE2B-E4D8F634367F"  -Name Attributes -Type DWord -Value 0 -Force | Out-Null
+	New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\501a4d13-42af-4429-9fd1-a8218c268e20\ee12f906-d277-404b-b6da-e5fa1a576df5"  -Name Attributes -Type DWord -Value 0 -Force | Out-Null
+	New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\9596FB26-9850-41fd-AC3E-F7C3C00AFD4B\34C7B99F-9A6D-4b3c-8DC7-B6693B78CEF4"  -Name Attributes -Type DWord -Value 0 -Force | Out-Null
+	New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\54533251-82be-4824-96c1-47b60b740d00\be337238-0d82-4146-a960-4f3749d470c7"  -Name Attributes -Type DWord -Value 0 -Force | Out-Null
+	print "Restored power options in Advanced power settings menu."
 }
 
 # Disable Autoplay.
