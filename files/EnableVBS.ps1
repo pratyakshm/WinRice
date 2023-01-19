f5c0929d4f6079259ab0a17fca1c28c0d3335266
Pratyaksh Mehrotra <contact@pratyakshm.com>
2023-01-20 00:55:36 +0530

    ♻️ Updated copyright year and email

--- files/EnableVBS.ps1
+++ files/EnableVBS.ps1
@@ -1,5 +1,5 @@
 # This file is a part of the WinRice software
-# Copyright (c) 2022 Pratyaksh Mehrotra <pratyakshm@protonmail.com>
+# Copyright (c) 2020-2023 Pratyaksh Mehrotra <contact@pratyakshm.com>
 # All rights reserved.
 
 Write-Host "Enabling Virtualization-based security..."
